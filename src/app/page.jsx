3f0a6d8e169a61c6e935797428cc0d50469698f7
sarahannie <sarahndianekwu@gmail.com>
2023-07-01 07:13:54 +0100

    corrected button

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Button from '@/component/button/button'
 
 export default function Home() {
   return (
@@ -11,11 +12,10 @@ export default function Home() {
       <p className={styles.desc}>
       With a simple button click, We bring your Ideas To Life, Turning them into Reality.
       </p>
-      <button className={styles.btn}>Contact</button>
-      
+      <Button url='/contact' text="Contact"/>
     </div>
     <div className={styles.item}>
-      <Image width={500} height={500} src="https://images.pexels.com/photos/17269311/pexels-photo-17269311/free-photo-of-fashion-people-woman-street.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" className={styles.img} />
+      <Image width={500} height={500} src="https://images.pexels.com/photos/17269311/pexels-photo-17269311/free-photo-of-fashion-people-woman-street.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="img_owner" className={styles.img} />
     </div>
   </div>
   )
---
+++ src/component/button/button.jsx
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+import React from 'react';
+import styles from './page.module.css'
+
+const Button = ({url, text}) => {
+  return (
+
+       <Link href={url}>
+        <button className={styles.container}>{text}</button>
+       </Link>
+        
+    
+  )
+}
+
+export default Button
